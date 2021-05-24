7e62a2adff059e174e29d73b270575fcb6ffa5d8
Silvia Gheorghita <thehungrysmurf@github.com>
2021-05-23 18:29:04 -0700

    Add 12-15 age category

--- generate_static_site.sh
+++ generate_static_site.sh
@@ -7,7 +7,7 @@ cp -r assets docs
 VACCINES=(pfizer moderna janssen)
 CATEGORIES=(flu-like gastrointestinal psychological life-threatening skin-and-localized-to-injection-site muscles-and-bones immune-system-and-inflammation nervous-system cardiovascular eyes-mouth-and-ears urinary breathing balance-and-mobility gynecological)
 SEXES=(male female)
-AGE_GROUPS=(16/25 26/39 40/59 60/75 76/89 90/110)
+AGE_GROUPS=(12/15 16/25 26/39 40/59 60/75 76/89 90/110)
 VAX_HOST=http://localhost:8888
 
 for VACCINE in ${VACCINES[@]}; do
